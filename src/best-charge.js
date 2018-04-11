d5bbe7d74a9a48b2f74f85f326f06b0088879e44
fanfan123 <1820148513@qq.com>
2018-04-11 18:49:59 +0800

    second

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,18 +1,17 @@
 function bestCharge(selectedItems) {
-  var allItems = loadAllItems();
-  var promote = loadPromotions();
-
   // getOrdered函数获得订餐明细
   function getOrdered(select, all) {
     var order_list =[];
     for(var i=0;i<select.length;i++){
       var temp = {};
+      var name;
+      var price;
       var id = select[i].split(" x ")[0];//菜品id
       var number = Number(select[i].split(" x ")[1]);//菜品数量
       for(var j=0;j<all.length;j++){
         if(all[j].id === id){
-          var name=all[j].name;
-          var price=all[i].price;
+          name=all[j].name;//菜品名
+          price=all[j].price;//菜品单价
           break;
         }
       }
@@ -20,9 +19,9 @@ function bestCharge(selectedItems) {
       temp.name=name;
       temp.number=number;
       temp.total_price=parseInt(number*price);//该菜品的总价格
-      order_list.push(temp)
+      order_list.push(temp);
     }
-    return order_list
+    return order_list;
   }
 
 //返回最优的价格
@@ -33,8 +32,8 @@ function bestCharge(selectedItems) {
     var price_result=[];
     for(var i=0;i<order_list.length;i++){
       total_price += order_list[i].total_price;
-      for(var j=0; j<promote.items.length;j++){
-        if(promote.items[j]===order_list[i].id){
+      for(var j=0; j<promote[1].items.length;j++){
+        if(promote[1].items[j]===order_list[i].id){
           half_price += order_list[i].total_price/2;
           half_name.push(order_list[i].name)
         }
@@ -45,19 +44,24 @@ function bestCharge(selectedItems) {
     if(total_price>=30){
       type1_price = total_price - 6;
     }
+    if(total_price<30 && half_price===0){
+      price_result.push("总计：" +total_price.toString()+"元")
+    }
     if(type1_price<=type2_price){
       var promote_type1 = promote[0].type + "，省6元";//优惠方式
       price_result.push(promote_type1);
       price_result.push("总计：" +type1_price.toString()+"元")
     }
     if(type1_price>type2_price){
-      var promote_type2 = promote[1].type+"("+half_name.toString()+")，省"+half_price.toString() +"元";
+      var promote_type2 = promote[1].type+"(";
+      for(var kk=0;kk<half_name.length-1;kk++){
+        promote_type2 += half_name[kk] + '，';
+      }
+      promote_type2 +=half_name[half_name.length-1]+")，省"+half_price.toString() +"元";
       price_result.push(promote_type2);
       price_result.push("总计：" +type2_price.toString()+"元")
     }
-    if(total_price<30 && half_price===0){
-      price_result.push("总计：" +total_price.toString()+"元")
-    }
+
     return price_result;
   }
 
@@ -65,7 +69,7 @@ function bestCharge(selectedItems) {
   function output(order_list, price_result) {
     var result ='============= 订餐明细 =============\n';
     for(var i=0;i<order_list.length;i++){
-      result += order_list[i].name + ' x ' + order_list[i].number.toString() + ' = '+order_list.total_price.toString()+'元\n';
+      result += order_list[i].name + ' x ' + order_list[i].number.toString() + ' = '+order_list[i].total_price.toString()+'元\n';
     }
     result += '-----------------------------------\n';
     if(price_result.length===2){
@@ -81,7 +85,9 @@ function bestCharge(selectedItems) {
     }
     return result;
   }
-
+  var allItems = loadAllItems();
+  var promote = loadPromotions();
+  console.log(promote);
   var order_list = getOrdered(selectedItems, allItems);
   var price_result = final_price(order_list, promote);
   var result = output(order_list,price_result);
